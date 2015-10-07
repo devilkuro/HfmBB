98f64a1c2bc40d3a979f72aa93d037bc72daf86c
Fanjing-LAB <devilkuro@gmail.com>
2015-10-07 15:21:21 +0800

    add SMTCarInfo class

---
+++ src/utils/SMTCarInfo.cc
@@ -0,0 +1,83 @@
+/*
+ * SMTCarInfo.cc
+ *
+ *  Created on: Oct 7, 2015
+ *      Author: Fanjing-LAB
+ */
+
+#include "SMTCarInfo.h"
+
+namespace Fanjing {
+string SMTCarInfo::path = "";
+XMLDocument* SMTCarInfo::doc = NULL;
+map<string, SMTCarInfo> SMTCarInfo::vTypeMap;
+SMTCarInfo::SMTCarInfo() {
+    // TODO Auto-generated constructor stub
+    id = "";
+    type = SMTCARINFO_ROUTETYPE_LAST_TYPE;
+    origin = "";
+    destination = "";
+    loop = "";
+    time = -1;
+    string vtype = "";
+
+    // type related
+    accel = -1;
+    decel = -1;
+    sigma = -1;
+    length = -1;
+    minGap = -1;
+    maxSpeed = -1;
+    color = "";   // useless
+}
+
+SMTCarInfo::~SMTCarInfo() {
+    // TODO Auto-generated destructor stub
+}
+
+void SMTCarInfo::loadVehicleTypeXML(string path) {
+    if(doc != NULL){
+        vTypeMap.clear();
+    }else{
+        doc = new XMLDocument();
+    }
+    XMLError e = doc->LoadFile(path.c_str());
+    if(e == XML_SUCCESS){
+        XMLElement* root = doc->FirstChildElement("routes");
+        XMLElement* e = root->FirstChildElement("vType");
+        SMTCarInfo car;
+        while(e){
+            if(NULL != e->Attribute("id")){
+                car.id = e->Attribute("id");
+                car.accel = e->DoubleAttribute("accel");
+                car.decel = e->DoubleAttribute("decel");
+                car.sigma = e->DoubleAttribute("sigma");
+                car.length = e->DoubleAttribute("length");
+                car.minGap = e->DoubleAttribute("minGap");
+                car.maxSpeed = e->DoubleAttribute("maxSpeed");
+                car.color = e->Attribute("color");
+                vTypeMap[e->Attribute("id")] = car;
+            }
+            e = e->NextSiblingElement("vType");
+        }
+    }
+    doc->Clear();
+}
+
+SMTCarInfo SMTCarInfo::getDefaultVeicleTypeInfo(string id) {
+    if(vTypeMap.find(id) != vTypeMap.end()){
+        return vTypeMap[id];
+    }
+    return SMTCarInfo();
+}
+
+list<string> SMTCarInfo::getDefaultVeicleTypeList() {
+    list<string> result;
+    for(map<string, SMTCarInfo>::iterator it = vTypeMap.begin();
+            it != vTypeMap.end(); it++){
+        result.push_back(it->first);
+    }
+    return result;
+}
+
+} /* namespace Fanjing */
---
+++ src/utils/SMTCarInfo.h
@@ -0,0 +1,65 @@
+/*
+ * SMTCarInfo.h
+ *
+ *  Created on: Oct 7, 2015
+ *      Author: Fanjing-LAB
+ */
+
+#ifndef SMTCARINFO_H_
+#define SMTCARINFO_H_
+
+#include <string>
+#include <map>
+#include <list>
+#include "tinyxml2.h"
+
+namespace Fanjing {
+
+/*
+ *
+ */
+
+using namespace std;
+using namespace tinyxml2;
+class SMTCarInfo {
+public:
+    enum RouteType {
+        SMTCARINFO_ROUTETYPE_OD = 0, // normal route: from origin to destination
+        SMTCARINFO_ROUTETYPE_LOOP,   // cyclic route: A-..-B-..-..-X(-..-A-..)
+        SMTCARINFO_ROUTETYPE_LAST_TYPE
+    };
+public:
+    SMTCarInfo();
+    virtual ~SMTCarInfo();
+
+    static void loadVehicleTypeXML(string path);
+    // return 0, if no car own this type id
+    static SMTCarInfo getDefaultVeicleTypeInfo(string vTypeId);
+    // load vehicle type xml first
+    static list<string> getDefaultVeicleTypeList();
+public:
+    string id;
+    RouteType type;
+    string origin;
+    string destination;
+    string loop;
+    double time;
+    string vtype;
+
+    // type related
+    double accel;
+    double decel;
+    double sigma;
+    double length;
+    double minGap;
+    double maxSpeed;
+    string color;   // useless
+
+private:
+    static string path;
+    static XMLDocument* doc;
+    static map<string,SMTCarInfo> vTypeMap;
+};
+
+} /* namespace Fanjing */
+#endif /* SMTCARINFO_H_ */
