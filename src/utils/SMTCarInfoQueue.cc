b966ef105b4fd8865d668b3e488a60a828f790b4
Fanjing-LAB <devilkuro@gmail.com>
2015-11-04 21:48:29 +0800

    2015-11-4

--- src/utils/SMTCarInfoQueue.cc
+++ src/utils/SMTCarInfoQueue.cc
@@ -22,7 +22,7 @@ bool SMTCarInfoQueue::overtakeAllowed = false;
 double SMTCarInfoQueue::updateInterval = 0.1;
 XMLDocument* SMTCarInfoQueue::doc = NULL;
 bool SMTCarInfoQueue::XMLHasLoaded = false;
-bool SMTCarInfoQueue::onlyLosseOneCar = true;
+bool SMTCarInfoQueue::onlyLosseOneCar = true;   // 是否每次仅松散一个车辆
 SMTCarInfoQueue::SMTCarInfoQueue() {
     // TODO Auto-generated constructor stub
     init();
@@ -69,6 +69,15 @@ void SMTCarInfoQueue::releaseXML() {
     }
 }
 
+void SMTCarInfoQueue::updateCarOutInfo(string id,string preId) {
+    // todo 更新车辆离开相关信息
+    // 1. 该功能需要完成的操作
+    //      a. 判定车辆启动并离开队列区的时间
+    //          该时间由当前车辆到达队列区时间和前方车辆开始驶离队列区时间共同影响决定
+    //      b. 判定驶离队列区时当前车辆在队列区的位置
+    //          该位置由当前车辆开始驶离时间所在通行周期内通过道路的车辆长度累加确定
+    //      c.
+}
 void SMTCarInfoQueue::updateCarQueueInfoAt(string id, string preId) {
     // 说明:
     // 1. 更新操作需要完成以下操作
@@ -93,6 +102,7 @@ void SMTCarInfoQueue::updateCarQueueInfoAt(string id, string preId) {
     //              b. 并且，当前车辆离开道路时，后方跟随车辆必须必须能够在接下来的updateInterval时间内能够抵达道路末尾
     //                  若无法抵达道路末尾，则表示后方跟随车辆通过道路的整个行程并未受到当前车辆的阻碍
     //              c. 判断完成,对后方跟随的车辆进行更新
+
     // todo 需要重写过程
     //1. 更新当前节点进入队列区的时间
     //      a. 查找前方车辆进入队列的时间
@@ -190,7 +200,7 @@ void SMTCarInfoQueue::setThePairMapAtFrontOfCar(map<double, list<string> >& carL
 
 void SMTCarInfoQueue::setThePairMapAtBackOfCar(map<double, list<string> >& carListMapByTime,
         map<string, double>& timeMapByCar, string id, string otherId) {
-    // todo 将id为id车辆插入到id为otherId的车辆的的后方
+    // 将id为id车辆插入到id为otherId的车辆的的后方
     // 0th. config function
     double time = -1;
     // 1st. record the time of other id
@@ -264,16 +274,6 @@ bool SMTCarInfoQueue::isCarACanOvertakeCarB(SMTCarInfo carA, SMTCarInfo carB, do
         // 在A和B同时进入的情况下,默认A无法超越B
         return enterTimeA < enterTimeB;
     }
-}
-
-void SMTCarInfoQueue::updateCarOutInfo(string id,string preId) {
-    // todo 更新车辆离开相关信息
-    // 1. 该功能需要完成的操作
-    //      a. 判定车辆启动并离开队列区的时间
-    //          该时间由当前车辆到达队列区时间和前方车辆开始驶离队列区时间共同影响决定
-    //      b. 判定驶离队列区时当前车辆在队列区的位置
-    //          该位置由当前车辆开始驶离时间所在通行周期内通过道路的车辆长度累加确定
-    //      c.
 }
 
 double SMTCarInfoQueue::getFixedTimeWithUpdateInterval(double time) {
